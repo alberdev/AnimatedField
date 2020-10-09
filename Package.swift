c762f25b2c24034ee1844b3f6d95f87b04770524
Murilo Araujo <muernandes@icloud.com>
2020-10-08 23:12:01 -0300

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,20 @@
+swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "AnimatedField",
+    products: [
+        .library(
+            name: "AnimatedField",
+            targets: ["AnimatedField"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "AnimatedField",
+            dependencies: [],
+            path: "AnimatedField",)
+    ]
+)
